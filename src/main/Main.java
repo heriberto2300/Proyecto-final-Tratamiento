92c5035dd0b2e921c84f8fa36246525e8ba1c7f2
Heriberto Alonso <joalonsom@hotmail.com>
2022-04-27 18:08:04 -0500

    Agregando nuevos conjuntos de datos, existen doubles en el conjunto, por lo que hay que cambiar de int

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,13 +16,19 @@ public class Main {
             Scanner x = new Scanner(System.in);
             /*Recuperacion de datos y construccion del conjunto de entrenamiento*/
             
-            ArrayList<int[]> entrenamiento = Files.leerDatos("datos-Sb/sb1-T.txt");
-            ArrayList<int[]> prueba = Files.leerDatos("datos-Sb/sb1-P.txt");
+            String t1 = "sb1-T.txt";
+            String p1 = "sb1-P.txt";
             
-            Datos datos = new Datos(entrenamiento, false);
+            String t2 = "aust1-T.txt";
+            String p2 = "aust1-P.txt";
+            
+            ArrayList<int[]> entrenamiento = Files.leerDatos("datos-Sb/aust1-T.txt");
+            ArrayList<int[]> prueba = Files.leerDatos("datos-Sb/aust1-P.txt");
+            
+            Datos datos = new Datos(entrenamiento, true);
             
             /*Clasficador KNN*/
-            KNN kVecinos = new KNN(datos, prueba, 5, false);
+            KNN kVecinos = new KNN(datos, prueba, 5, true);
             
             Thread threadKNN = new Thread(kVecinos);
             
@@ -30,8 +36,6 @@ public class Main {
             int[] b = {2, 1, 2, 1, 2};
             
             System.out.println(kVecinos.hvdm(a, b));*/
-            
-            /*TODO: VALIDAR SI ES MODA O MEDIA PARA LOS VECINOS MAS CERCANOS*/
             
             threadKNN.start();
             
@@ -45,15 +49,19 @@ public class Main {
             
             String nombreT = "sb1-T.arff";
             String nombreP = "sb1-P.arff";
-            String cabecera = Files.initCabeceraARFF(datos.getTipoAtributos(), datos.getCabecera(), datos.getTotalClases(), nombreT);
-            Files.crearARFF(entrenamiento, cabecera, nombreT);
-            Files.crearARFF(prueba, cabecera, nombreP);
+            
+            String nombreT2 = "aust1-T.arff";
+            String nombreP2 = "aust1-P.arff";
+            
+            
+            String cabecera = Files.initCabeceraARFF(datos.getTipoAtributos(), datos.getCabecera(), datos.getTotalClases(), nombreT2);
+            Files.crearARFF(entrenamiento, cabecera, nombreT2);
+            Files.crearARFF(prueba, cabecera, nombreP2);
             
             System.out.println("\nFinalizado. Presionar cualquier tecla para continuar...");
             x.nextLine();
             
             /*Clasificador C4.5*/
-            System.out.println("\n----------CONSTRUCCION DE DATOS EN FORMATO ARFF PARA WEKA---------\n");
 
             Arbol arbol = new Arbol(nombreT, nombreP);
             Thread threadArbol = new Thread(arbol);
